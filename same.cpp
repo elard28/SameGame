b646cae755c1f07278d4207147de9be1763f392d
Elard Guevara <elard.guevara@ucsp.edu.pe>
2015-04-06 15:45:05 -0500

    parametros del main

--- same.cpp
+++ same.cpp
@@ -5,7 +5,18 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	SameGame sg("matrix.txt");
+	if(argc<2)
+    {
+        cout<<"sin archivo que tomar"<<endl;
+        return 0;
+    }
+
+    string ar=argv[1];
+
+    char *tex=new char[ar.length()+1];
+    strcpy(tex,ar.c_str());
+
+	SameGame sg(tex);
 
     sg.show();
     sg.lets_play();
